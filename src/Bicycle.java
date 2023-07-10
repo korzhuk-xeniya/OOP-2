1796096015a36f81a0fc0197cade63d4afaf63df
Ксения <korzhuk.xeniya@yandex.ru>
2023-07-10 09:59:43 +0300

    Пименено наследование и инкапсуляция до просмотра видео урока

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,8 +1,7 @@
-public class Bicycle {
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+public class Bicycle extends TransportVehicle {
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
+
+
 }
--- src/TransportVehicle.java
+++ src/TransportVehicle.java
@@ -1,2 +1,29 @@
-public class TransportVehicle {
+public abstract class TransportVehicle {
+    private String modelName;
+    private int wheelsCount;
+
+    public TransportVehicle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
 }
